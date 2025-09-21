2a590a98fb0c8eca8052ea452cfb008827d194be
cacao-sheen <cacaoquennie@gmail.com>
2025-09-21 17:49:02 +0800

    Initial commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12799498',
+    'password'	=> '6w7HDhNn3L',
+    'database'	=> 'sql12799498',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
